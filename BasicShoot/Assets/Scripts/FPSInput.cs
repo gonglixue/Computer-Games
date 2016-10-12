7ffc313e4da41189d56cbe1577cc981420f6355a
Gong Lixue <724351282@qq.com>
2016-10-12 15:38:34 +0800

    Create FPSInput.cs

---
+++ BasicShoot/Assets/Scripts/FPSInput.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+[AddComponentMenu("Control Script/FPS Input")]
+
+public class FPSInput : MonoBehaviour {
+    public float speed = 6.0f;
+    public float gravity = -9.8f;
+
+    private CharacterController _characterController;
+	// Use this for initialization
+	void Start () {
+        _characterController = GetComponent<CharacterController>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        float deltaZ = Input.GetAxis("Vertical") * speed;
+
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        movement = Vector3.ClampMagnitude(movement, speed);
+
+       
+
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        movement.y = gravity;
+        _characterController.Move(movement);
+	
+	}
+}
